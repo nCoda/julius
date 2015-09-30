b468a474c7be4a21e004c1f8f3435f2d915d5768
Christopher Antila <christopher@antila.ca>
2015-09-30 01:13:04 -0400

    Fixed: problem with devserver where the CSS file wasn't generated properly
    Previously, the "less_autocompile" script would only recompile the LESS file
    when it was changed, even if it had never been compiled before (i.e., the
    even when the CSS file was missing). Now that script compiles the LESS file
    whenever it's started, then it starts watching the file and recompiles on
    changes.

--- less_autocompiler.sh
+++ less_autocompiler.sh
@@ -1,4 +1,9 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
+# do an initial compile
+node_modules/less/bin/lessc --clean-css $1 $2
+
+# now set up the file-watch
 while inotifywait -e modify -qq $1
 do
     node_modules/less/bin/lessc --clean-css $1 $2
