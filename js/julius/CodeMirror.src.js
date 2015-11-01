560a2d7c91f7a18a37bab0e5af2e4b12b1a15534
Christopher Antila <christopher@antila.ca>
2015-11-01 14:05:46 -0500

    CodeMirror now uses the version in "node_modules"

--- js/julius/CodeMirror.src.js
+++ js/julius/CodeMirror.src.js
@@ -11,8 +11,9 @@
 // https://github.com/JedWatson/react-codemirror/blob/f0c0fc50d2226f4757ccae4a7736ca9911902d9d/src/Codemirror.js
 
 import React from "react";
-import CodeMirror from "../codemirror/lib/codemirror.js";
-import "../codemirror/mode/python/python.js";
+import CodeMirror from 'codemirror';
+// TODO: figure out how to do this without cheating like this
+import '../../node_modules/codemirror/mode/python/python.js';
 
 
 var ReactCodeMirror = React.createClass({
