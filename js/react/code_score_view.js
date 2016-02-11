69a94ee8d885bc6f52644dee147c55cbafd01cc5
Sienna <sienna.m.wood@gmail.com>
2016-02-11 16:29:07 -0700

    react-split-pane implemented for CodeScoreView

--- js/react/code_score_view.js
+++ js/react/code_score_view.js
@@ -6,7 +6,7 @@
 // Filename:               js/react/code_score_view.js
 // Purpose:                React components for CodeScoreView.
 //
-// Copyright (C) 2015 Christopher Antila, Wei Gao
+// Copyright (C) 2015, 2016 Christopher Antila, Wei Gao, Sienna M. Wood
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -26,44 +26,12 @@ import {Button, ButtonGroup} from 'amazeui-react';
 import React from "react";
 import ReactDOM from 'react-dom';
 import ReactCodeMirror from "./CodeMirror";
+import SplitPane from '../../node_modules/react-split-pane/lib/SplitPane';
+
 
 import getters from '../nuclear/getters';
 import reactor from '../nuclear/reactor';
 import signals from '../nuclear/signals';
-
-
-/** This function handles resizing elements separated by a Separator component.
- *
- * @param {int} doThis - Move the element by this many pixels. (0, 0) is at the top-left.
- * @param {str} thisDirection - Either "horizontal" or "vertical," depending on the intended
- *     direction of movement. Note this is the *opposite* of the Separator component's "direction" prop.
- * @param {Element} zeroElem - The element closer to "zero" in the direction of movement. For vertical
- *     movement, this is the higher Element; for horizontal movement this is the Element on the left.
- * @param {Element} oneElem - The other element being resized.
- * @returns {undefined}
- */
-function handleSeparator(doThis, thisDirection, zeroElem, oneElem) {
-    // get the existing span
-    let zeroMagnitude;
-    let oneMagnitude;
-    if ('vertical' === thisDirection) {
-        zeroMagnitude = zeroElem.offsetHeight;
-        oneMagnitude = oneElem.offsetHeight;
-    }
-    else {
-        zeroMagnitude = zeroElem.offsetWidth;
-        oneMagnitude = oneElem.offsetWidth;
-    }
-
-    // do the adjustment
-    zeroMagnitude += doThis;
-    oneMagnitude -= doThis;
-
-    // set everything
-    const dimension = ('vertical' === thisDirection) ? 'height' : 'width';
-    zeroElem.style[dimension] = `${zeroMagnitude}px`;
-    oneElem.style[dimension] = `${oneMagnitude}px`;
-}
 
 
 const TextEditor = React.createClass({
@@ -93,13 +61,15 @@ const TextEditor = React.createClass({
             smartIndent: true,
             electricChars: true,
             lineNumbers: true,
+            autofocus: true,
+            lineWrapping: true,
             inputStyle: "contenteditable",  // NOTE: this usually defaults to "textarea" on
                                             // desktop and may not be so good for us, but it has
                                             // better IME and and screen reader support
         };
         return (
             <div className="ncoda-text-editor">
-                <h2>{`Text Editor`}</h2>
+                <h3>{`Text Editor`}</h3>
                 <ReactCodeMirror
                     path="ncoda-editor"
                     options={codeMirrorOptions}
@@ -187,7 +157,7 @@ const Verovio = React.createClass({
     },
     render() {
         const innerHtml = {__html: this.renderWithVerovio(this.state.meiForVerovio)};
-        return <div className="ncoda-verovio" ref="verovioFrame" dangerouslySetInnerHTML={innerHtml}/>;
+        return <div className="ncoda-verovio" ref="verovioFrame" dangerouslySetInnerHTML={innerHtml}></div>;
     },
 });
 
@@ -201,25 +171,16 @@ const WorkTable = React.createClass({
     getDefaultProps() {
         return {meiForVerovio: ''};
     },
-    handleSeparator(doThis, thisDirection) {
-        handleSeparator(
-            doThis,
-            thisDirection,
-            ReactDOM.findDOMNode(this.refs.textEditor),
-            ReactDOM.findDOMNode(this.refs.verovio)
-        );
-    },
     render() {
         return (
-            <div ref="workTable" className="ncoda-work-table">
+            <SplitPane split="vertical" ref="workTable" className="ncoda-work-table">
                 <TextEditor
                     ref="textEditor"
                     submitToPyPy={this.props.submitToPyPy}
                     submitToLychee={this.props.submitToLychee}
                 />
-                <Separator direction="vertical" onMove={this.handleSeparator} />
-                <Verovio ref="verovio" meiForVerovio={this.props.meiForVerovio} />
-            </div>
+                <div><Verovio ref="verovio" meiForVerovio={this.props.meiForVerovio} /></div>
+            </SplitPane>
         );
     },
 });
@@ -267,7 +228,7 @@ const TerminalWindow = React.createClass({
             className += `${className} ${this.props.extraClass}`;
         }
         return (
-            <div className={className} dangerouslySetInnerHTML={innerHtml}/>
+            <div className={className} dangerouslySetInnerHTML={innerHtml}></div>
         );
     },
 });
@@ -279,107 +240,21 @@ const TerminalOutput = React.createClass({
     getDataBindings() {
         return {stdout: getters.stdout, stderr: getters.stderr, stdin: getters.stdin};
     },
-    handleSeparator(doThis, thisDirection) {
-        handleSeparator(
-            doThis,
-            thisDirection,
-            ReactDOM.findDOMNode(this.refs.theLeftBox),
-            ReactDOM.findDOMNode(this.refs.theRightBox)
-        );
-    },
     render() {
         return (
-            <div id="ncoda-terminal-output" className="ncoda-terminal-output">
-                <h3><div>{`Your Input`}</div><div>{`Python Output`}</div></h3>
-                <div className="ncoda-output-terminals">
-                    <TerminalWindow outputThis={this.state.stdin} ref="theLeftBox" />
-                    <Separator direction="vertical" onMove={this.handleSeparator} />
-                    <TerminalWindow outputThis={this.state.stdout} ref="theRightBox" />
-                </div>
+        <SplitPane split="vertical" id="ncoda-terminal-output" className="ncoda-terminal-output">
+            <div>
+                <h3>{`Your Input`}</h3>
+                <TerminalWindow outputThis={this.state.stdin} />
             </div>
+            <div>
+                <h3>{`Python Output`}</h3>
+                <TerminalWindow outputThis={this.state.stdout} />
+            </div>
+        </SplitPane>
         );
     },
 });
-
-
-const Separator = React.createClass({
-    // TODO: let the caller submit two @id attributes, on which we'll set min- and max- properties,
-    //       different depending on whether it's horizontal or vertical. And without those props,
-    //       the resize cursor won't be shown.
-    propTypes: {
-        direction: React.PropTypes.oneOf(["horizontal", "vertical"]),
-        extraCssClass: React.PropTypes.string,  // to add a CSS class to this Separator
-        onMove: React.PropTypes.func,  // TODO: write explanation about this
-    },
-    getDefaultProps() {
-        return {direction: "horizontal", extraCssClass: null};
-    },
-    getInitialState() {
-        // - "mouseDown": set to "true" when the mouse is down
-        return {mouseDown: false, recentestObservation: null};
-    },
-    handleMouseMove(event) {
-        if (this.state.mouseDown && this.props.onMove) {
-            const state = {};
-            let direction = null;  // this will be opposite of the Separator's direction
-            if ('vertical' === this.props.direction) {
-                state.recentestObservation = event.clientX;
-                direction = 'horizontal';
-            }
-            else {
-                state.recentestObservation = event.clientY;
-                direction = 'vertical';
-            }
-            const magnitude = state.recentestObservation - this.state.recentestObservation;
-            this.setState(state);
-            this.props.onMove(magnitude, direction );
-        }
-    },
-    handleMouseDown(event) {
-        // make sure we start in the right place
-        const state = {mouseDown: true};
-        if ('vertical' === this.props.direction) {
-            state.recentestObservation = event.clientX;
-        }
-        else {
-            state.recentestObservation = event.clientY;
-        }
-
-        // subscribe to MouseEvent events so we can process the dragging
-        this.refs.thePlane.addEventListener('mousemove', this.handleMouseMove);
-        this.refs.thePlane.addEventListener('mouseup', this.handleMouseUp);
-
-        // set the "mouse down" CSS classes and recentestObservation
-        this.setState(state);
-    },
-    handleMouseUp(event) {
-        // unsubscribe to MouseEvent events
-        this.refs.thePlane.removeEventListener(MouseEvent, this.mouseEventMultiplexer);
-
-        // make sure we stop in the right place
-        this.handleMouseMove(event);
-
-        // set the "mouse up" CSS classes
-        this.setState({mouseDown: false});
-    },
-    render() {
-        let className = `nc-separator nc-separator-${this.props.direction}`;
-        const planeStyle = {display: 'none'};
-        if (null !== this.props.extraCssClass) {
-            className = `${className} ${this.props.extraCssClass}`;
-        }
-        if (this.state.mouseDown) {
-            className = `${className} nc-separator-selected`;
-            planeStyle.display = 'block';
-        }
-        return (
-            <div className={className} onMouseDown={this.handleMouseDown}>
-                <div ref="thePlane" className="nc-separator-plane" style={planeStyle}/>
-            </div>
-        );
-    },
-});
-
 
 const CodeScoreView = React.createClass({
     propTypes: {
@@ -394,25 +269,18 @@ const CodeScoreView = React.createClass({
             sendToConsoleType: 'welcome',
         };
     },
-    handleSeparator(doThis, thisDirection) {
-        handleSeparator(
-            doThis,
-            thisDirection,
-            ReactDOM.findDOMNode(this.refs.workTable),
-            ReactDOM.findDOMNode(this.refs.terminalOutput)
-        );
-    },
     render() {
         return (
             <div id="nc-csv-frame">
-                <WorkTable
-                    ref="workTable"
-                    submitToPyPy={signals.emitters.submitToPyPy}
-                    submitToLychee={signals.emitters.submitToLychee}
-                    meiForVerovio={this.props.meiForVerovio}
-                />
-                <Separator onMove={this.handleSeparator}/>
-                <TerminalOutput ref="terminalOutput"/>
+                <SplitPane split="horizontal" minSize="20">
+                    <WorkTable
+                        ref="workTable"
+                        submitToPyPy={signals.emitters.submitToPyPy}
+                        submitToLychee={signals.emitters.submitToLychee}
+                        meiForVerovio={this.props.meiForVerovio}
+                    />
+                    <TerminalOutput ref="terminalOutput"/>
+                </SplitPane>
             </div>
         );
     },
