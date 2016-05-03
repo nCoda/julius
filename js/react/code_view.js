00698af9296c0bf6a85da010ce3b9ec3110843e6
Sienna <sienna.m.wood@gmail.com>
2016-05-03 11:40:39 -0600

    new SubmitCodeButton class to modularize buttons that 'submit as lilypond' and 'run as python'

--- js/react/code_view.js
+++ js/react/code_view.js
@@ -27,8 +27,75 @@ import React from 'react';
 import CodeMirror from './codemirror';
 import {Button, ButtonGroup} from 'amazeui-react';
 import Scroll from './scroll';
-import {IconPython, IconLilypond} from './svg_icons';
+import {IconPython, IconLilypond, IconCoda} from './svg_icons';
 
+
+const SubmitCodeButton = React.createClass({
+    propTypes: {
+        onClick: React.PropTypes.func.isRequired,
+        codeLanguage: React.PropTypes.string.isRequired,
+        displayText: React.PropTypes.string,
+        hoverText: React.PropTypes.string,
+        logo: React.PropTypes.bool
+    },
+    getDefaultProps() {
+        return {
+            displayText: '',
+            hoverText: '',
+            logo: false
+        };
+    },
+    whichLogo() {
+        let logo = this.props.logo;
+        let codeLanguage = this.props.codeLanguage.toLowerCase();
+        if (logo === true) {
+            if (codeLanguage === "python") {
+                return <IconPython />;
+            } else if (codeLanguage === "lilypond") {
+                return <IconLilypond />;
+            } else {
+                return <IconCoda />; // backup logo is coda symbol
+            }
+        } else {
+            return;
+        }
+    },
+    whichTitle() {
+        let hoverText = this.props.hoverText;
+        if (hoverText !== ''){
+            return hoverText;
+        } else { // if hoverText is not defined, generate default text
+            hoverText = "Submit " + this.props.codeLanguage.toUpperCase();
+            return hoverText;
+        }
+    },
+    whichText() {
+        let logo = this.props.logo;
+        let displayText = this.props.displayText;
+        let hoverText = this.props.hoverText;
+        if (logo === true) {
+            return displayText;         // can be an empty string if logo is present
+        } else {                        // if no logo, make sure displayText is not an empty string
+            if (displayText !== '') {   // use displayText if defined
+                return displayText;
+            } else {                    // if displayText not defined, use hoverText if defined
+                if (hoverText !== ''){
+                    return hoverText;
+                } else {                // if hoverText not defined, generate default text
+                    displayText = "Submit " + this.props.codeLanguage.toUpperCase();
+                    return displayText;
+                }
+            }
+        }
+    },
+    render() {
+        return (
+            <Button title={this.whichTitle()} className="am-btn-xs" onClick={this.props.onClick}>
+                {this.whichLogo()}{this.whichText()}
+            </Button>
+        );
+    },
+});
 
 export const CodeView = React.createClass({
     propTypes: {
@@ -47,14 +114,35 @@ export const CodeView = React.createClass({
     handleEditorChange(withThis) {
         this.setState({editorValue: withThis});
     },
+    handleSubmitPython() {
+        this.props.submitToPyPy(this.state.editorValue);
+    },
+    handleSubmitLilyPond() {
+        this.props.submitToLychee(this.state.editorValue, 'lilypond');
+    },
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextState.editorValue !== this.state.editorValue) {
+            return true;
+        }
+        return false;
+    },
     render() {
         return (
             <div>
                 <div className="pane-head">
                     <h2>{this.props.title}</h2>
-                    <CodeViewButtons submitToPyPy={this.props.submitToPyPy}
-                                     submitToLychee={this.props.submitToLychee}
-                                     editorValue={this.state.editorValue}/>
+                    <div className="ncoda-text-editor-controls">
+                        <ButtonGroup>
+                            <SubmitCodeButton codeLanguage="python"
+                                              onClick={this.handleSubmitPython}
+                                              logo={true}
+                                              hoverText="Run as Python" />
+                            <SubmitCodeButton codeLanguage="lilypond"
+                                              onClick={this.handleSubmitLilyPond}
+                                              logo={true}
+                                              hoverText="Submit as Lilypond" />
+                        </ButtonGroup>
+                    </div>
                 </div>
                 <Scroll>
                     <CodeMirror
@@ -64,32 +152,4 @@ export const CodeView = React.createClass({
             </div>
         );
     },
-});
-
-const CodeViewButtons = React.createClass({
-    propTypes: {
-        submitToLychee: React.PropTypes.func.isRequired,
-        submitToPyPy: React.PropTypes.func.isRequired,
-        editorValue: React.PropTypes.string.isRequired,
-    },
-    handleSubmitPython() {
-        this.props.submitToPyPy(this.props.editorValue);
-    },
-    handleSubmitLilyPond() {
-        this.props.submitToLychee(this.props.editorValue, 'lilypond');
-    },
-    render() {
-        return (
-            <div className="ncoda-text-editor-controls">
-                <ButtonGroup>
-                    <Button title="Run as Python" className="am-btn-xs" onClick={this.handleSubmitPython}>
-                        <IconPython />
-                    </Button>
-                    <Button title="Submit as Lilypond" className="am-btn-xs" onClick={this.handleSubmitLilyPond}>
-                        <IconLilypond />
-                    </Button>
-                </ButtonGroup>
-            </div>
-        )
-    }
 });
