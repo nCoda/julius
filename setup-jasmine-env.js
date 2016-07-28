caa64b9963a7557b9b03aa9b0162fd32d35a8ab6
Christopher Antila <christopher@antila.ca>
2016-07-27 20:00:38 -0400

    Tests: generate JUnit XML results for CircleCI

---
+++ setup-jasmine-env.js
@@ -0,0 +1,13 @@
+// If we're running on CircleCI, generate JUnit XML results.
+
+if (process.env.CIRCLE_TEST_REPORTS) {
+    var jasmineReporters = require('jasmine-reporters');
+    // jasmine.VERBOSE = true;
+    jasmine.getEnv().addReporter(
+        new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            savePath: process.env.CIRCLE_TEST_REPORTS + '/jest',
+            filePrefix: 'test-results',
+        })
+    );
+}
