5ef0106234d519b02371b7e7ac97eaa43fc7bb22
Sienna <sienna.m.wood@gmail.com>
2016-10-07 22:46:26 -0600

    Create/Fix: Tests for DialogueBox
    Fix existing tests and create new tests to reflect use of the amazeUI modal component.
    Structure tested with a shallow render; click, callback, and dismiss/hide tested with a mount.
    
    For T27

--- js/react/tests/test_generics.js
+++ js/react/tests/test_generics.js
@@ -6,7 +6,7 @@
 // Filename:               js/react/tests/test_generics.js
 // Purpose:                Tests for the "generics" React components.
 //
-// Copyright (C) 2016 Christopher Antila
+// Copyright (C) 2016 Christopher Antila, Sienna M. Wood
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -23,30 +23,17 @@
 //-------------------------------------------------------------------------------------------------
 
 import {init} from '../../nuclear/init';
-
 import nuclear from 'nuclear-js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 
 import reactor from '../../nuclear/reactor';
 import signals from '../../nuclear/signals';
 import getters from '../../nuclear/getters';
 
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+
 jest.dontMock('../generics');
 const generics = require('../generics');
-
-
-describe('ModalBackground', () => {
-    it('works as intended', () => {const children = 'this is the children';
-        const backg = TestUtils.renderIntoDocument( <generics.ModalBackground children={children}/> );
-        const backgNode = ReactDOM.findDOMNode(backg);
-
-        expect(backgNode.tagName).toBe('DIV');
-        expect(backgNode.className).toBe('nc-modal-background');
-        expect(backgNode.innerHTML).toBe(children);
-    });
-});
 
 
 describe('DialogueBox', () => {
@@ -54,67 +41,58 @@ describe('DialogueBox', () => {
 
     it('works properly when not displayed', () => {
         signals.emitters.dialogueBoxHide();
-        const box = TestUtils.renderIntoDocument( <generics.DialogueBox/> );
-        const boxNode = ReactDOM.findDOMNode(box);
-        expect(boxNode.tagName).toBe('DIV');
-        expect(boxNode.style.display).toBe('none');
+        const dialogueBox = shallow(<generics.DialogueBox/>).node;
+        expect(dialogueBox.ref).toBe('dialogueBoxHidden');
+        expect(dialogueBox.type).toBe('div');
+        expect(dialogueBox.props.style.display).toBe('none');
     });
 
     /** Run a test on the DialogueBox component
      *
      * @param {object} props - Object with type, message, detail, callback.
-     * @param {string} expIconClass - The expected className of the <i> icon; this string will have
-     *        "fa fa-" prepended automatically, so you can omit that part.
-     * @param {string} expTitle - The text expected in the "nc-dialogue-type" <div>.
-     * @param {string} answer - The text to write into the <input> element (only for "question").
+     * @param {string} answer - The text to write into the <SimpleInput> element (only for "question" type).
      * @returns {undefined}
      */
-    function dialogueBoxTester(props, expIconClass, expTitle, answer) {
+    function dialogueBoxTester(props, answer) {
         signals.emitters.dialogueBoxShow(props);
-        const box = TestUtils.renderIntoDocument( <generics.DialogueBox/> );
-        const boxNode = ReactDOM.findDOMNode(box);
 
-        // check that it rendered correctly
-        expect(boxNode.tagName).toBe('DIV');
-        expect(boxNode.className).toBe('nc-modal-background');
-        expect(boxNode.children[0].tagName).toBe('DIV');
-        expect(boxNode.children[0].className).toBe(`nc-dialogue-box nc-dialogue-${props.type}`);
-        const titleDiv = boxNode.children[0].children[0];
-        expect(titleDiv.tagName).toBe('DIV');
-        expect(titleDiv.className).toBe('nc-dialogue-type');
-        expect(titleDiv.children[0].tagName).toBe('I');
-        expect(titleDiv.children[0].className).toBe(`fa fa-${expIconClass}`);
-        expect(titleDiv.children[1].tagName).toBe('H3');
-        expect(titleDiv.children[1].innerHTML).toBe(expTitle);
-        const message = boxNode.children[0].children[1];
-        expect(message.tagName).toBe('P');
-        expect(message.className).toBe('nc-dialogue-msg');
-        expect(message.innerHTML).toBe(props.message);
-        const detail = boxNode.children[0].children[2];
-        expect(detail.tagName).toBe('P');
-        expect(detail.className).toBe('nc-dialogue-detail');
-        expect(detail.innerHTML).toBe(props.detail);
-        let button;
-        if (answer) {
-            // 3 is the <input> for "answer"
-            button = boxNode.children[0].children[4];
-        }
-        else {
-            button = boxNode.children[0].children[3];
-        }
-        expect(button.tagName).toBe('BUTTON');
-        expect(button.className).toBe('btn');
-        expect(button.innerHTML).toBe('OK');
+        // check that nuclear is calling this object correctly
+        expect(reactor.evaluate(getters.DialogueBox).get('displayed')).toBe(true);
+        expect(reactor.evaluate(getters.DialogueBox).get('type')).toEqual(props.type);
+        expect(reactor.evaluate(getters.DialogueBox).get('message')).toEqual(props.message);
+        expect(reactor.evaluate(getters.DialogueBox).get('detail')).toEqual(props.detail);
 
-        // write text in the <input> answer box
+        // shallow render and check structure
+        const dialogueBoxWrapper = shallow(<generics.DialogueBox/>);
+        const dialogueBox = dialogueBoxWrapper.node;
+
+        expect(dialogueBox.type.displayName).toBe('Modal');
+        const h1 = dialogueBoxWrapper.find('h1');
+        expect(h1.node.props.children).toBe(props.message);
+        const paragraph = dialogueBoxWrapper.find('p');
+        expect(paragraph.node.props.children).toBe(props.detail);
+
         if (props.type === 'question') {
-            boxNode.children[0].children[3].value = answer;
+            const simpleInput = dialogueBoxWrapper.find('SimpleInput');
+            expect(simpleInput.node.ref).toBe('dialogueBoxInput');
+        } else {
+            const div = dialogueBoxWrapper.find('div');
+            expect(div.node.ref).toBe('dialogueBoxHidden');
+            expect(div.node.props.style.display).toBe('none');
         }
 
-        // "click" the OK button
-        TestUtils.Simulate.click(button);
+        // mount to simulate click and check callback function
+        const dialogueBoxMount = mount(<generics.DialogueBox />);
 
-        // check that the callback was called, and the DialogueBox hidden
+        //put text in input if question type
+        if(props.type == 'question') {
+            dialogueBoxMount.node.handleInputChange(answer);
+        }
+
+        // simulate click
+        dialogueBoxMount.find('Modal').node.props.onRequestClose();
+
+        // check that the callback was called correctly and the DialogueBox was hidden
         if (props.type === 'question') {
             expect(props.callback).toBeCalledWith(answer);
         } else {
@@ -125,37 +103,27 @@ describe('DialogueBox', () => {
 
     it('works with "error"', () => {
         const props = {type: 'error', message: 'A', detail: 'B', callback: jest.genMockFn()};
-        const expIconClass = 'exclamation-triangle';
-        const expTitle = 'Error';
-        dialogueBoxTester(props, expIconClass, expTitle);
+        dialogueBoxTester(props);
     });
 
     it('works with "warn"', () => {
         const props = {type: 'warn', message: 'A', detail: 'B', callback: jest.genMockFn()};
-        const expIconClass = 'exclamation-circle';
-        const expTitle = 'Warning';
-        dialogueBoxTester(props, expIconClass, expTitle);
+        dialogueBoxTester(props);
     });
 
     it('works with "info"', () => {
         const props = {type: 'info', message: 'A', detail: 'B', callback: jest.genMockFn()};
-        const expIconClass = 'info-circle';
-        const expTitle = 'Information';
-        dialogueBoxTester(props, expIconClass, expTitle);
+        dialogueBoxTester(props);
     });
 
     it('works with "debug"', () => {
         const props = {type: 'debug', message: 'A', detail: 'B', callback: jest.genMockFn()};
-        const expIconClass = 'bug';
-        const expTitle = 'Developer Message';
-        dialogueBoxTester(props, expIconClass, expTitle);
+        dialogueBoxTester(props);
     });
 
     it('works with "question"', () => {
         const props = {type: 'question', message: 'A', detail: 'B', callback: jest.genMockFn()};
-        const expIconClass = 'question-circle';
-        const expTitle = 'Question';
         const answer = '42';
-        dialogueBoxTester(props, expIconClass, expTitle, answer);
+        dialogueBoxTester(props, answer);
     });
 });
