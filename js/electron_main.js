8a1fa033426f6788b468bca0a620b36930f4a1c4
Sienna <sienna.m.wood@gmail.com>
2016-10-29 16:27:02 -0600

    Fix: Enable Cut and Paste Functionality in OSX
    Added 'Edit' menu and commands to allow cut-and-paste operations in OSX.

--- js/electron_main.js
+++ js/electron_main.js
@@ -55,6 +55,36 @@ app.on('ready', function () {
 
 // Menus
 let template = [{
+    label: 'Edit',
+    role: 'edit',
+    submenu: [{
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+    }, {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+    }, {
+        type: 'separator'
+    }, {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+    }, {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+    }, {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+    }, {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+    }]
+}, {
     label: 'View',
     submenu: [{
         label: 'Reload (Cmd/Ctrl R)',
