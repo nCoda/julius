cfba74ccb1ff7af0e049ac18ae786a6eddbeafc4
Christopher Antila <christopher@antila.ca>
2016-12-28 17:46:38 -0500

    Remove: "vcs" Lychee outbound registration in ncoda-init
    We're not using VCS functionality for now, so this is unnecessary.

--- js/ncoda-init.js
+++ js/ncoda-init.js
@@ -52,7 +52,6 @@ signals.emitters.fujianStartWS();
 // (Runs only when the WebSocket connection is ready).
 signals.emitters.lyInitializeSession();
 signals.emitters.registerOutboundFormat('document', 'ncoda-init', false);
-signals.emitters.registerOutboundFormat('vcs', 'ncoda-init', false);
 
 
 // Render the react-router components -----------------------------------------
